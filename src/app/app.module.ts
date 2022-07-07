a12b964b2d6252cd6918b7034e825abb2f3f8c1d
Nav-ee-n <naveenfelix876@gmail.com>
2022-07-07 23:49:05 +0530

    Created View-Games

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddgamesComponent } from './addgames/addgames.component';
+import { ViewGamesComponent } from './view-games/view-games.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddgamesComponent
+    AddgamesComponent,
+    ViewGamesComponent
   ],
   imports: [
     BrowserModule,
